2036af930fad3ad18c68a4725a10ac52942684f1
Manish <manmanisharma@gmail.com>
2022-10-08 20:49:50 +0530

    added paginated api

--- app.js
+++ app.js
@@ -10,10 +10,45 @@ app.get('/',async (req,res)=>{
     res.render('index',{videos:[]});
     
 })
+app.get('/search',async (req,res) => {
+    const searchTerm=req.body.query;
+   const newurl = `${Url}/search?key=${API_KEY}&type=video&part=snippet&q=${searchTerm}&order=date&maxResults=50`;
+    const response = await axios.get(newurl);
+    
+    const videos = response.data.items;
+     const newdata=[]
+     videos.forEach(video => {
+            newdata.push(video.snippet)
+     })
+    
+        const page = parseInt(req.query.page)
+        const limit =parseInt( req.query.limit)
+      
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+    
+        const results = {}
+    
+        if (endIndex < 50) {
+          results.next = {
+            page: page + 1,
+            limit: limit
+          }
+        }
+        
+        if (startIndex > 0) {
+          results.previous = {
+            page: page - 1,
+            limit: limit
+          }
+        }
+        results.results=newdata.slice(startIndex,endIndex)
+    res.send(results)
+
+})
 app.post('/search',async (req,res) => {
     const searchTerm=req.body.query;
-console.log(searchTerm);
-    const newurl = `${Url}/search?key=${API_KEY}&type=video&part=snippet&q=${searchTerm}&order=date`;
+   const newurl = `${Url}/search?key=${API_KEY}&type=video&part=snippet&q=${searchTerm}&order=date&maxResults=50`;
     const response = await axios.get(newurl);
     
     const videos = response.data.items;
